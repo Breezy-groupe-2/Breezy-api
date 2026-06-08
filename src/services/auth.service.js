9876a65d7d030ff2a156efae4e98de4b0248cb7a
Christ YEO <cdognimain@gmail.com>
2026-06-08 11:42:09 +0200

    feat(auth): handle duplicate email or username

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,7 +3,20 @@ const User = require('../models/user.model');
 
 const register = async ({ username, email, password }) => {
   const passwordHash = await bcrypt.hash(password, 12);
-  const user = await User.create({ username, email, passwordHash });
+
+  let user;
+  try {
+    user = await User.create({ username, email, passwordHash });
+  } catch (err) {
+    if (err.code === 11000) {
+      const field = Object.keys(err.keyPattern)[0];
+      const error = new Error(`${field} already taken`);
+      error.status = 409;
+      throw error;
+    }
+    throw err;
+  }
+
   return {
     id: user._id,
     username: user.username,
