06fe88144f5d07fa57d84b1f1aea93dfa9f4b61e
Christ YEO <cdognimain@gmail.com>
2026-06-08 09:49:33 +0200

    hash passwords before saving

--- auth-service/src/controllers/authController.js
+++ auth-service/src/controllers/authController.js
@@ -1,10 +1,11 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
-const sequelize = require('../config/database');
 
 const register = async (req, res) => {
   const { username, email, password } = req.body;
 
-  const user = await User.create({ username, email, password_hash: password });
+  const password_hash = await bcrypt.hash(password, 12);
+  const user = await User.create({ username, email, password_hash });
 
   res.status(201).json({
     id: user.id,
