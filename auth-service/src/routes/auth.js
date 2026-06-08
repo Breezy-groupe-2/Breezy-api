f3ffd42b62db5be0b54a3d08e4759d854b9ac4dc
Christ YEO <cdognimain@gmail.com>
2026-06-08 09:49:13 +0200

    validate registration input

--- auth-service/src/routes/auth.js
+++ auth-service/src/routes/auth.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const { register } = require('../controllers/authController');
+const { validate, registerSchema } = require('../middleware/validate');
 
 const router = Router();
 
-router.post('/register', register);
+router.post('/register', validate(registerSchema), register);
 
 module.exports = router;
