8dca64633847c14c9051ac0d723ca4e746604dc4
Christ YEO <cdognimain@gmail.com>
2026-06-09 16:15:16 +0200

    feat(feed): add chronological feed endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userRoutes = require('./routes/user/user.routes');
 const postRoutes = require('./routes/post/post.routes');
+const feedRoutes = require('./routes/feed/feed.routes');
 const { me } = require('./controllers/auth.controller');
 const { authenticate } = require('./middlewares/authenticate');
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use('/api/v1/auth', userRoutes);
 app.get('/api/v1/users/me', authenticate, me);
 app.use('/api/v1/posts', postRoutes);
+app.use('/api/v1/feed', feedRoutes);
 
 app.use((err, _req, res, _next) => {
   console.error(err);
