e78c248a06f64bf69335724646a46fc13ce6e675
Christ YEO <cdognimain@gmail.com>
2026-06-09 16:16:20 +0200

    feat(feed): return posts from followed users

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -30,6 +30,7 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: true,
     },
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   },
   { timestamps: true }
 );
