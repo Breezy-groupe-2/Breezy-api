7e8a781826047fe610f2265d5458c05fa5e104e4
Dognimain Christ Yeo <cdognimain@gmail.com>
2026-06-09 15:35:27 +0200

    Feature/fx2 authentication (#102)
    Co-authored-by: SliiZ-code <julien.petitcolboc@gmail.com>

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,11 +2,29 @@ const authService = require('../services/auth.service');
 
 const register = async (req, res, next) => {
   try {
-    const user = await authService.register(req.body);
-    res.status(201).json(user);
+    const result = await authService.register(req.body);
+    res.status(201).json(result);
   } catch (err) {
     next(err);
   }
 };
 
-module.exports = { register };
+const login = async (req, res, next) => {
+  try {
+    const result = await authService.login(req.body);
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const me = async (req, res, next) => {
+  try {
+    const user = await authService.getMe(req.user.sub);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { register, login, me };
