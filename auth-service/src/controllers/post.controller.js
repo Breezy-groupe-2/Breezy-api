ff9c08a141a9faeb26f3d6e595952a05023dddf2
Christ YEO <cdognimain@gmail.com>
2026-06-12 11:01:22 +0200

    feat(posts): list posts by user endpoint

--- auth-service/src/controllers/post.controller.js
+++ auth-service/src/controllers/post.controller.js
@@ -1,5 +1,14 @@
 const postService = require('../services/post.service');
 
+const getPostsByUser = async (req, res, next) => {
+  try {
+    const posts = await postService.getPostsByUser(req.params.userId);
+    res.status(200).json(posts);
+  } catch (err) {
+    next(err);
+  }
+};
+
 const createPost = async (req, res, next) => {
   try {
     const post = await postService.createPost({ content: req.body.content, authorId: req.user.sub });
@@ -22,4 +31,4 @@ const updatePost = async (req, res, next) => {
   }
 };
 
-module.exports = { createPost, updatePost };
+module.exports = { createPost, updatePost, getPostsByUser };
--- auth-service/src/services/post.service.js
+++ auth-service/src/services/post.service.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Post = require('../models/post.model');
 const Like = require('../models/like.model');
 
@@ -24,4 +25,19 @@ const updatePost = async ({ postId, content, authorId }) => {
   return { id: post._id, content: post.content, author: post.author, updatedAt: post.updatedAt, likeCount };
 };
 
-module.exports = { createPost, updatePost };
+const getPostsByUser = async (userId) => {
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    const err = new Error('User not found');
+    err.status = 404;
+    throw err;
+  }
+  const posts = await Post.find({ author: userId }).sort({ createdAt: -1 });
+  return Promise.all(
+    posts.map(async (post) => {
+      const likeCount = await Like.countDocuments({ post: post._id });
+      return { id: post._id, content: post.content, author: post.author, createdAt: post.createdAt, likeCount };
+    }),
+  );
+};
+
+module.exports = { createPost, updatePost, getPostsByUser };
