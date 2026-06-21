717759dfc982f165b1e2c79f63af26445e15726e
SliiZ-code <julien.petitcolboc@gmail.com>
2026-06-21 12:14:46 +0000

    feat(feed): fetch followed posts in one batch

--- feed-service/src/controllers/feed.controller.js
+++ feed-service/src/controllers/feed.controller.js
@@ -1,9 +1,26 @@
 const feedService = require('../services/feed.service');
 
+const parseLimit = (queryLimit) => {
+  if (queryLimit === undefined) {
+    return 20;
+  }
+
+  const limit = Number(queryLimit);
+  if (!Number.isInteger(limit)) {
+    const err = new Error('limit must be an integer');
+    err.status = 400;
+    throw err;
+  }
+
+  return Math.min(Math.max(limit, 1), 100);
+};
+
 const getFeed = async (req, res, next) => {
   try {
+    const limit = parseLimit(req.query.limit);
     const posts = await feedService.getFeed(req.user.sub, {
       authorization: req.headers.authorization,
+      limit,
     });
     res.status(200).json(posts);
   } catch (err) {
@@ -11,4 +28,4 @@ const getFeed = async (req, res, next) => {
   }
 };
 
-module.exports = { getFeed };
+module.exports = { getFeed, parseLimit };
